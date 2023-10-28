b92ace054e4ddd37a05ecea1c833eb0960020fc4
Shriya <102695770+2110992105@users.noreply.github.com>
2023-10-28 21:26:30 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".home-slider",{
+    grabCursor:true,
+    loop:true,
+    cnteredSlides:true,
+    navigation:{
+        nextEl: ".swipper-button-next",
+        prevEl: ".swipper-button-prev",
+    },
+
+
+});
